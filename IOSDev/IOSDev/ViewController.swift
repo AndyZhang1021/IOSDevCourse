b2cde55fa486032400ed5b29232c8e4236d79967
Andy <Andy@mail.com>
2021-04-29 12:16:16 +1000

    create a new xcode project

---
+++ IOSDev/IOSDev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSDev
+//
+//  Created by Andy Zhang on 29/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
